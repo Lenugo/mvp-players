55c45e0810a8fa8d1f25906d2576786056f47260
Lenugo <lnunez@intellisys.com.do>
2021-10-08 10:07:37 -0400

    add players styles in players component

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -9,9 +9,10 @@ import { PagesComponent } from "./pages.component";
 import { AmericanComponent } from "./american/american.component";
 import { NationalComponent } from "./national/national.component";
 import { PlayersComponent } from "../components/players/players.component";
+import { HeaderComponent } from "../shared/header/header.component";
 
 @NgModule({
-  declarations: [PagesComponent, HomeComponent, AmericanComponent, NationalComponent, PlayersComponent],
+  declarations: [PagesComponent, HomeComponent, AmericanComponent, NationalComponent, PlayersComponent, HeaderComponent],
   imports: [CommonModule, RouterModule, PagesRoutingModule],
   exports: [PagesComponent, HomeComponent],
 })
