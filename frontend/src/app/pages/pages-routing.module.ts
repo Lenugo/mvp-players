af4c68b8d4b07816b3e6d23c3e86947bc6536cb4
Lenugo <lnunez@intellisys.com.do>
2021-10-13 23:07:56 -0400

    add route profile

--- frontend/src/app/pages/pages-routing.module.ts
+++ frontend/src/app/pages/pages-routing.module.ts
@@ -4,12 +4,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { AmericanComponent } from "./american/american.component";
 import { HomeComponent } from "./home/home.component";
 import { NationalComponent } from "./national/national.component";
-import { PagesComponent } from "./pages.component";
+import { ProfileComponent } from "./profile/profile.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "american", component: AmericanComponent },
   { path: "national", component: NationalComponent },
+  { path: "profile", component: ProfileComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
 ];
 
